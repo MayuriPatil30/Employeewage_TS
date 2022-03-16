dfb692b81d0982102be08094c151b47c59b4ff57
MayuriPatil30 <patilmayu.mp@gmail.com>
2022-03-17 00:03:14 +0530

    Employee using Switch case Program

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,15 +3,16 @@ var Is_Present = 1;
 var Rate_Per_Hr = 20;
 var Fday_Hr = 8;
 var Part_Hr = 4;
-var empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == 1) {
-    console.log("Emplyoee is Present");
-    console.log("Daily wage is " + (Rate_Per_Hr * Fday_Hr));
-}
-else if (empCheck == 2) {
-    console.log("Emplyoee is Present");
-    console.log("Part time daily wage:" + (Rate_Per_Hr * Part_Hr));
-}
-else {
-    console.log("Emplyoee is absent");
+var empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck) {
+    case 1:
+        console.log("Emplyoee is Present");
+        console.log("Daily wage is " + (Rate_Per_Hr * Fday_Hr));
+        break;
+    case 2:
+        console.log("Emplyoee is Present");
+        console.log("Part time daily wage:" + (Rate_Per_Hr * Part_Hr));
+        break;
+    default:
+        console.log("Emplyoee is absent");
 }
--- EmployeeWage.ts
+++ EmployeeWage.ts
@@ -6,18 +6,19 @@ var Rate_Per_Hr: number = 20;
 var Fday_Hr: number = 8;
 var Part_Hr: number = 4;
 
-var empCheck: number = Math.floor(Math.random() * 10) % 2;
+var empCheck: number = Math.floor(Math.random() * 10) % 3;
 
-if (empCheck == 1) {
-    console.log("Emplyoee is Present");
-    console.log("Daily wage is " + (Rate_Per_Hr * Fday_Hr));
-
-} else if (empCheck == 2) {
-    console.log("Emplyoee is Present");
-    console.log("Part time daily wage:" + (Rate_Per_Hr * Part_Hr))
-}
-else {
-    console.log("Emplyoee is absent");
+switch (empCheck) {
+    case 1:
+        console.log("Emplyoee is Present");
+        console.log("Daily wage is " + (Rate_Per_Hr * Fday_Hr));
+        break;
+    case 2:
+        console.log("Emplyoee is Present");
+        console.log("Part time daily wage:" + (Rate_Per_Hr * Part_Hr))
+        break;
+    default:
+        console.log("Emplyoee is absent");
 }
 
 
