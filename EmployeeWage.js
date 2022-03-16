770ea7ea4c9a76d3366074afdae7949412a8cb17
MayuriPatil30 <patilmayu.mp@gmail.com>
2022-03-16 23:26:03 +0530

    Employee Daily Wage Program

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,14 @@
 console.log("Welcome to Employee Wage Computation");
 var Is_Present = 1;
+var Rate_Per_Hr = 20;
+var Fday_Hr = 8;
+var Total_DailyWadge;
+Total_DailyWadge = Rate_Per_Hr * Fday_Hr;
 var empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == Is_Present) {
     console.log("Emplyoee is Present");
+    console.log("Daily wedge is " + Total_DailyWadge);
 }
 else {
-    console.log("Emplyoee is Absent");
+    console.log("Emplyoee is absent");
 }
--- EmployeeWage.ts
+++ EmployeeWage.ts
@@ -1,10 +1,24 @@
 
 console.log("Welcome to Employee Wage Computation");
 
-var Is_Present : number = 1;
-var empCheck : number = Math.floor(Math.random()*10)%2;
-if(empCheck==Is_Present){
+var Is_Present: number = 1;
+var Rate_Per_Hr: number = 20;
+var Fday_Hr: number = 8;
+var Total_DailyWadge: number;
+
+Total_DailyWadge = Rate_Per_Hr * Fday_Hr;
+var empCheck: number = Math.floor(Math.random() * 10) % 2;
+
+if (empCheck == Is_Present) {
     console.log("Emplyoee is Present");
-}else{
-    console.log("Emplyoee is Absent");
+    console.log("Daily wedge is " + Total_DailyWadge);
+
+} else {
+    console.log("Emplyoee is absent");
+
 }
+
+
+
+
+
