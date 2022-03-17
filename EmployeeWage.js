970959b1dd1fae0e65aad5a6de79fc779bc5f5dc
MayuriPatil30 <patilmayu.mp@gmail.com>
2022-03-17 11:49:09 +0530

    Calculate Employee Monthly Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,18 +1,30 @@
-console.log("Welcome to Employee Wage Computation");
+console.log("\n Welcome to Employee Wage Computation \n");
 var Is_Present = 1;
-var Rate_Per_Hr = 20;
-var Fday_Hr = 8;
-var Part_Hr = 4;
-var empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case 1:
-        console.log("Emplyoee is Present");
-        console.log("Daily wage is " + (Rate_Per_Hr * Fday_Hr));
-        break;
-    case 2:
-        console.log("Emplyoee is Present");
-        console.log("Part time daily wage:" + (Rate_Per_Hr * Part_Hr));
-        break;
-    default:
-        console.log("Emplyoee is absent");
+var RATE_PER_HR = 20;
+var FDAY_HR = 8;
+var PART_HR = 4;
+var No_of_day = 20;
+var daywage = 0;
+var monthwage = 0;
+var empHr = 0;
+var totalEmpHr = 0;
+for (No_of_day = 0; No_of_day < 20; No_of_day++) {
+    var empCheck = (Math.floor(Math.random() * 10) % 3);
+    switch (empCheck) {
+        case 1:
+            console.log("Emplyoee is Present And Full Time Daily wage is: " + FDAY_HR);
+            empHr = FDAY_HR;
+            break;
+        case 2:
+            console.log("Emplyoee is Present And Part Time Daily Wage is: " + PART_HR);
+            empHr = PART_HR;
+            break;
+        default:
+            console.log("Emplyoee is absent");
+            empHr = 0;
+    }
+    totalEmpHr = totalEmpHr + empHr;
+    console.log("Total Employee Hr is :" + totalEmpHr);
 }
+monthwage = RATE_PER_HR * totalEmpHr;
+console.log("\n Monthly wage is : " + monthwage, "\n");
--- EmployeeWage.ts
+++ EmployeeWage.ts
@@ -1,25 +1,40 @@
 
-console.log("Welcome to Employee Wage Computation");
+console.log("\n Welcome to Employee Wage Computation \n");
 
 var Is_Present: number = 1;
-var Rate_Per_Hr: number = 20;
-var Fday_Hr: number = 8;
-var Part_Hr: number = 4;
+const RATE_PER_HR: number = 20;
+const FDAY_HR: number = 8;
+const PART_HR: number = 4;
+var No_of_day: number = 20;
+var daywage : number = 0;
+var monthwage : number = 0;
+var empHr: number = 0;
+var totalEmpHr : number = 0;
 
-var empCheck: number = Math.floor(Math.random() * 10) % 3;
+for (No_of_day = 0; No_of_day < 20; No_of_day++) {
+    var empCheck: number = (Math.floor(Math.random() * 10) % 3);
 
-switch (empCheck) {
-    case 1:
-        console.log("Emplyoee is Present");
-        console.log("Daily wage is " + (Rate_Per_Hr * Fday_Hr));
-        break;
-    case 2:
-        console.log("Emplyoee is Present");
-        console.log("Part time daily wage:" + (Rate_Per_Hr * Part_Hr))
-        break;
-    default:
-        console.log("Emplyoee is absent");
+    switch (empCheck) {
+        case 1:
+            console.log("Emplyoee is Present And Full Time Daily wage is: " + FDAY_HR);
+            empHr = FDAY_HR;
+            break;
+
+        case 2:
+            console.log("Emplyoee is Present And Part Time Daily Wage is: " + PART_HR);
+            empHr = PART_HR;
+
+            break;
+        default:
+          
+            console.log("Emplyoee is absent");
+            empHr = 0;
+    }
+     totalEmpHr = totalEmpHr + empHr;
+     console.log("Total Employee Hr is :" + totalEmpHr);
 }
+   monthwage = RATE_PER_HR * totalEmpHr;
+   console.log("\n Monthly wage is : " + monthwage, "\n");
 
 
 
